b50fa8942a3773c32d41b35f2d3326d12dccd2fe
Annette Wöhler <annettewoehler@gmx.de>
2024-01-31 18:53:12 +0100

    + Add challenge solution

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,18 @@
-import { createServer } from "node:http";
+// server.js
+import { createServer } from "http";
+import Chance from "chance";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  const chance = new Chance();
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+  const responseString = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+  if (request.url === "/") {
+    response.end(responseString);
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+});
--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,5 +1,10 @@
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
-  response.end("Hello, Netti!");
+  if (request.url === "/") {
+    response.end("Hello, Netti!");
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
 });
