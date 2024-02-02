9d75e8dda8a22098dd3d8761edb24fb691e6b4f0
Annette Wöhler <annettewoehler@gmx.de>
2024-02-02 11:16:14 +0100

    - Remove statement for simplification

--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,10 +1,6 @@
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
-  if (request.url === "/") {
-    response.end("Hello, Netti!");
-  } else {
-    response.statusCode = 404;
-    response.end("Not Found");
-  }
+  response.statusCode = 200;
+  response.end("Hello, Netti!");
 });
