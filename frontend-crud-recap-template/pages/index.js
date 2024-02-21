f1e90be8f6ef618a20ed074fbf1ebf3b7a7bd933
Annette Wöhler <annettewoehler@gmx.de>
2024-02-21 12:06:48 +0100

    + Add photo list

--- frontend-crud-recap-template/pages/index.js
+++ frontend-crud-recap-template/pages/index.js
@@ -1,9 +1,16 @@
-// name your list resource in the page props
-// and map over it to render it
-export default function IndexPage() {
+import Link from "next/link";
+
+export default function IndexPage({ photos }) {
   return (
     <>
       <h1>Frontend Recap</h1>
+      <ul>
+        {photos.map(({ id, title }) => (
+          <Link href={`/photos/${id}`}>
+            <li key={id}>{title}</li>
+          </Link>
+        ))}
+      </ul>
     </>
   );
 }
