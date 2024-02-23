fffc78217bf48ad630c9c1a22d4290972af4883e
Annette Wöhler <annettewoehler@gmx.de>
2024-02-23 22:07:48 +0100

    + Add PhotoList component

--- frontend-crud-recap-template/pages/index.js
+++ frontend-crud-recap-template/pages/index.js
@@ -1,16 +1,10 @@
-import Link from "next/link";
+import PhotoList from "@/components/PhotoList";
 
-export default function IndexPage({ photos }) {
+export default function IndexPage({ photos = [] }) {
   return (
     <>
       <h1>Frontend Recap</h1>
-      <ul>
-        {photos.map(({ id, title }) => (
-          <Link href={`/photos/${id}`}>
-            <li key={id}>{title}</li>
-          </Link>
-        ))}
-      </ul>
+      {!photos.length ? "No photos yet." : <PhotoList photos={photos} />}
     </>
   );
 }
