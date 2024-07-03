96521417729d96c3c85ef00759c6a4e5764551e8
Annette Wöhler <annettewoehler@gmx.de>
2024-07-03 18:22:33 +0200

    * Improve solution

--- react-data-fetching_star-wars/pages/characters/[id].js
+++ react-data-fetching_star-wars/pages/characters/[id].js
@@ -1,43 +1,31 @@
-import useSWR from "swr";
-import { useRouter } from "next/router";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
 
 export default function Character() {
   const router = useRouter();
   const { id } = router.query;
-  const { data, isLoading, error } = useSWR(
+  const {
+    data: character,
+    isLoading,
+    error,
+  } = useSWR(
     `https://swapi.dev/api/people/${id}`,
     null // fetcher wird hier nicht übergeben, da er global in SWRConfig konfiguriert ist
   );
 
-  if (isLoading) {
-    return (
-      <Layout>
-        <h1>Loading...</h1>
-      </Layout>
-    );
-  }
-
-  if (error) {
-    return (
-      <Layout>
-        <h1>{error.status}</h1>
-        <p>Error: {error.message}</p>
-      </Layout>
-    );
-  }
-
-  const { name, height, eye_color, birth_year } = data;
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 
   return (
     <Layout>
       <Card
         id={id}
-        name={name}
-        height={height}
-        eyeColor={eye_color}
-        birthYear={birth_year}
+        name={character.name}
+        height={character.height}
+        eyeColor={character.eye_color}
+        birthYear={character.birth_year}
       />
     </Layout>
   );
