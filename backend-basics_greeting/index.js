6c86f5af19c8bea026b4ec950ddd793186e4e3b3
Annette Wöhler <annettewoehler@gmx.de>
2024-07-04 17:07:27 +0200

    * Update solution

--- backend-basics_greeting/index.js
+++ backend-basics_greeting/index.js
@@ -1,6 +1,5 @@
 import { server } from "./server.js";
 
-const port = 8000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(8000, () => {
+  console.log("Server up and running on http://localhost:8000");
 });
