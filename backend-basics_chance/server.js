b2f5fea62d9ae4212a2717c2cd5a3f86b330fd72
Annette Wöhler <annettewoehler@gmx.de>
2024-07-04 17:22:08 +0200

    * Update solution

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,17 +1,14 @@
-import { createServer } from "http";
-import Chance from "chance";
+import { Chance } from "chance";
+import { createServer } from "node:http";
+
+const chance = new Chance();
 
 export const server = createServer((request, response) => {
-  const chance = new Chance();
   const name = chance.name();
   const age = chance.age();
   const profession = chance.profession();
-  const responseString = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
 
-  if (request.url === "/") {
-    response.end(responseString);
-  } else {
-    response.statusCode = 404;
-    response.end("Not Found");
-  }
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
 });
